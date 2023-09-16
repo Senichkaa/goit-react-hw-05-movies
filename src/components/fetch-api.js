0f191d876bc43910a42cd3a7c41a0c4aeedb8a85
Senichkaa <super-kapanaiko406@ukr.net>
2023-09-16 11:39:25 +0300

    added axios and fetch-api functions

---
+++ src/components/fetch-api.js
@@ -0,0 +1,36 @@
+import axios from "axios";
+
+const BASE_URL = 'https://api.themoviedb.org/3';
+const API_KEY = 'f178f7ce976ed1c53f42d6d8f6a9420b'
+
+export async function trendingRequest() {
+    const { data } = await axios.get(`${BASE_URL}/trending/movie/day?api_key=${API_KEY}&language=en-US`)
+    
+    return data;
+}
+
+export async function searchMovieRequest() {
+    const { data } = await axios.get(`${BASE_URL}/search/movie?api_key=${API_KEY}&include_adult=false&language=en-US&page=1&query=${film}`)
+    
+    return data;
+}
+
+export async function movieDetailsRequest() {
+    const { data } = await axios.get(`${BASE_URL}/movie/${movieId}?api_key=${API_KEY}&language=en-US`)
+    
+    return data;
+}
+
+export async function movieCastRequest() {
+    const { data } = await axios.get(`${BASE_URL}/movie/${movieId}/credits?api_key=${API_KEY}&language=en-US`)
+    
+    return data;
+}
+
+export async function movieReviewsRequest() {
+    const { data } = await axios.get(`${BASE_URL}/movie/${movieId}/reviews?api_key=${API_KEY}&language=en-US&page=1`)
+    
+    return data;
+}
+
+
